67c64b0c2fa12c802abcdf709f5cc69dae35b709
krlos <pckrlos@gmail.com>
2022-11-03 15:11:33 -0400

    dev config to connect to mongo DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development'],
+    isGlobal: true,
+  }), MongooseModule.forRoot(process.env.URI_MONGODB, {
+    useCreateIndex: true,
+    UseFindAndModify: false,
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
